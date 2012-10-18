f922a5cb831947dd3d974e08ace24f9f2f98fce7
Karthik Raja <k.raja@accenture.com>
2012-10-18 16:00:21 +0530

    pilot

--- javascripts/search-content.js
+++ javascripts/search-content.js
@@ -56,6 +56,7 @@ return outMonth;
 function expand(id){
 
 	$(".content").show();
+	$('.firstdiv').css('background-color', '#FFFFFF');
 	$('#div_'+id).css('background-color', '#E6E6E6');
 	console.log("Expand Row Id::: "+ id);
 	
