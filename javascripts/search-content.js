4d5fc9ff2a1df45fc6752a1370a17b1976173344
mamatha <munivelu.mamatha@accenture.com>
2012-10-18 10:08:27 +0530

    Update javascripts/search-content.js

--- javascripts/search-content.js
+++ javascripts/search-content.js
@@ -128,7 +128,8 @@ function search() {
                     //discussion +='<p line-height:70%>';
                    // discussion +='<ol>';
 		    discussion +='<ul>';
-                    discussion +='<li class="discussion"><a href="'+url+'"target="_apps">'+subject+'</a> <button type="button" style="float: right;"><a href="'+url+'"target="_apps">Expand</a></button></li>';
+                   // discussion +='<li class="discussion"><a href="'+url+'"target="_apps">'+subject+'</a> <button type="button" style="float: right;" ><a href="'+url+'"target="_apps">Expand</a></button></li>';
+                    discussion +='<li class="discussion">'+subject+'</a> <button type="button" style="float: right;" <a href="'+url+'"target="_apps">Expand</a></button></li>';
                     discussion +='</ul>';  
                     
                     discussion +='<font size="2" color="grey">';               
