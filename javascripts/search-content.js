4e13e8daef02552eeb17499fbb43c2358aed925e
Karthik Raja <k.raja@accenture.com>
2012-10-18 14:50:19 +0530

    pilot

--- javascripts/search-content.js
+++ javascripts/search-content.js
@@ -135,7 +135,7 @@ function search() {
                    // discussion +='<ol>';
 					discussion +='<ul>';
                     discussion +='<li class="discussion"><a href="'+url+'" target="_apps">'+subject+'</a></li>';
-                    discussion +='<li ><input type="image" class="image-button" id="'+discussionID+'" value="" ></li>';
+                    discussion +='<li class="image-button" id="'+discussionID+'" ></li>';
                     //discussion +=<button type="button" style="float: right;" <a href="'+url+'"target="_apps">Expand</a></button></li>';
                     discussion +='</ul>';  
                     
