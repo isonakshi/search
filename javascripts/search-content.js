ce7e58751c02d8a31f631c710d10fdde8db6dba8
Karthik Raja <k.raja@accenture.com>
2012-10-18 17:31:12 +0530

    pilot

--- javascripts/search-content.js
+++ javascripts/search-content.js
@@ -95,7 +95,7 @@ function expand(id){
 	
 	
 	});
-	console.log("Html Content"discussionexpand);
+	console.log("Html Content:: "+discussionexpand);
 	$(".content").show();
 	 $(".content").html(discussionexpand);
 	
