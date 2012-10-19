7adf72ad88a2c9e0fc46874d928b44480b029f94
Karthik Raja <k.raja@accenture.com>
2012-10-19 14:21:28 +0530

    pilot

--- javascripts/search-content.js
+++ javascripts/search-content.js
@@ -79,7 +79,7 @@ function expand(id){
 				console.log("I'm inside Root Message Div");
 				rootmessage +='<div>';
 				rootmessage +='<ul>';
-				rootmessage +='<li >'+row.messages.root.subject+'</a></li>';
+				rootmessage +='<li >'+messages.root.subject+'</a></li>';
 				rootmessage +='</ul>';
 				rootmessage +='</div>';
 				
